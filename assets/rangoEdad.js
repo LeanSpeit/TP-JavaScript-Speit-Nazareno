d18c586247923d477b41edf96158802621db0604
Speit <nazarenovillalba14@gmail.com>
2024-05-03 09:53:47 -0300

    se realizaron 2/4 ejercicios

---
+++ assets/rangoEdad.js
@@ -0,0 +1,10 @@
+let edad = parseInt(prompt("Ingrese una edad"))
+
+if (edad >= 0 && edad <= 12){
+    console.log("Según la edad, es un niño")
+} else if (edad >= 13 && edad <= 19){
+    console.log("Según la edad, es un adolescente")
+} else if (edad >= 20 && edad <= 59){
+    console.log("Según la edad, es un adulto")
+} else if (hora == 60)
+    console.log("Según la edad, es un adulto mayor")
