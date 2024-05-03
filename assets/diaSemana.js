b5b48d01a69093e435007a58fe18261b453934ca
Speit <nazarenovillalba14@gmail.com>
2024-05-03 10:35:02 -0300

    se finalizaron los 4 ejercicios

---
+++ assets/diaSemana.js
@@ -0,0 +1,26 @@
+let dia = parseInt(prompt("Ingrese una dia"))
+
+if (dia == 1) {
+    console.log("Lunes")
+}
+if (dia == 2) {
+    console.log("Martes")
+}
+if (dia == 3) {
+    console.log("Miercoles")
+}
+if (dia == 4) {
+    console.log("Jueves")
+}
+if (dia == 5) {
+    console.log("Viernes")
+}
+if (dia == 6) {
+    console.log("Sabado")
+}
+if (dia == 7) {
+    console.log("Domingo")
+}
+if (dia > 7) {
+    console.log("Ingrese un día válido")
+}
